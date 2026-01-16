37fc684c28284603613f91b44dabbae49dc9df7a
MikelGascon <santistebangas@gmail.com>
2026-01-16 13:44:44 +0100

    cambios con todo añadido por el momento

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 // Raiz main del proyecto
-define("BASE_URL", 'http://localhost/CorteRebelde/ProyectoFinalTienda/');
+define("BASE_URL", 'http://localhost/CorteRebelde/ProyectoFinalTienda');
 
 define("PAG_URL", '/public');
 define("CSS_URL", '/src/css');
 define("JS_URL","/src/Js");
 define("COMPONENTS_URL","/src/components");
-define("ENTITY_ULR","/src/Entity");
+define("ENTITY_URL","/src/Entity");
 define("IMG_URL","/src/img");
 define("PROCESS_URL","/src/procesos");
 
--- src/Entity/TallaRopa.php
+++ src/Entity/TallaRopa.php
@@ -1,13 +1,39 @@
 <?php
-namespace App\Entity;
-use Doctrine\ORM\Mapping as ORM;
 
-/** @ORM\Entity @ORM\Table(name="tallaRopa") */
-class TallaRopa {
-    /** @ORM\Id @ORM\GeneratedValue @ORM\Column(type="integer") */
-    private $id;
-    /** @ORM\Column(type="string", name="nombre") */
-    private $nombre;
-    public function getId() { return $this->id; }
-    public function getNombre() { return $this->nombre; }
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+
+#[ORM\Entity]
+#[ORM\Table(name: "tallaRopa")]
+class TallaRopa 
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, name: "nombre")]
+    private ?string $nombre = null;
+
+    // --- GETTERS ---
+
+    public function getId(): ?int 
+    { 
+        return $this->id; 
+    }
+
+    public function getNombre(): ?string 
+    { 
+        return $this->nombre; 
+    }
+
+    // --- SETTERS ---
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
 }
